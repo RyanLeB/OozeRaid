1616848b9ac8acb60273404ab4e697c4f62fe83a
Ryan LeBlanc <122310498+RyanLeB@users.noreply.github.com>
2024-09-23 09:42:36 -0300

    some assets imported

---
+++ Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursro : MonoBehaviour
+{
+
+    [SerializeField] private Texture2D cursorTexture;
+
+
+    private Vector2 cursorHotSpot;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cursorHotSpot = new Vector2(cursorTexture.width / 2, cursorTexture.height / 2);
+        Cursor.SetCursor(cursorTexture, cursorHotSpot, CursorMode.Auto);
+
+    }
+
+    
+}
